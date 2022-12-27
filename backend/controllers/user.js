7b672e58872cb6c19ca40d6507b0bcbca5834040
Babas788 <agi.bastien@hotmail.fr>
2022-12-27 13:28:11 +0100

    valid

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -28,7 +28,9 @@ exports.login = (req, res, next) => {
       }
       bcrypt.compare(req.body.password, user.password).then((valid) => {
         if (!valid) {
-          return res.status(401).json({ error: "Mot de passe incorrect !" });
+          return res
+            .status(401)
+            .json({ error: "Paire identifiant / mot de passe incorrecte !" });
         }
         res.status(200).json({
           userId: user._id,
